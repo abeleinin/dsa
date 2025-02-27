783cc78f28f7c897da879cb482af35b48e0f6207
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-02-26 23:53:51 -0600

    leetcode 3

--- neetcode150/medium/3-Longest-Substring-Without-Repeating-Characters.cpp
+++ neetcode150/medium/3-Longest-Substring-Without-Repeating-Characters.cpp
@@ -3,15 +3,27 @@
 
 using namespace std;
 
-class Solution {
+/**
+ * \c Solution1
+ * 
+ * 1. Check insertion into a set to determine when a character collision occurs.
+ * 2. You don't know the position of the conflicting char, so you have to use while not equal to determine location
+ * 3. Incremenet both i and j when coflict otherwise just j
+ * 4. Use (int)chars.size() to determine current substring length
+ * 
+ * Time  : O(n)
+ * Space : O(n)
+ */
+class Solution1 {
 public:
     int lengthOfLongestSubstring(string s) {
-        set<char> chars = {};
+        int res = 0;
+        set<char> chars;
+
         int i = 0;
         int j = 0;
-        int res = 0;
         while (j < s.size()){
-            auto [it, inserted] = chars.insert(s[j]);
+            auto [_, inserted] = chars.insert(s[j]);
             if (!inserted) {
                 while (s[i] != s[j]) {
                     chars.erase(s[i]);
