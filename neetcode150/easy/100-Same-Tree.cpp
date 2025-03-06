bb9845c701d3134dec4f6bc475e2d3d2f39d93a5
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-03-05 23:57:18 -0600

    leetcode 100

--- neetcode150/easy/100-Same-Tree.cpp
+++ neetcode150/easy/100-Same-Tree.cpp
@@ -10,7 +10,10 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+/**
+ * \c Solution1
+ */
+class Solution1 {
 public:
     bool dfs(TreeNode* p, TreeNode* q) {
         if (p == NULL && q == NULL) {
@@ -25,6 +28,28 @@ public:
         bool left = dfs(p->left, q->left);
         bool right = dfs(p->right, q->right);
         return left && right;
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return dfs(p, q);
+    }
+};
+
+/**
+ * \c Solution2
+ */
+class Solution2 {
+public:
+    bool dfs(TreeNode* p, TreeNode* q) {
+        if (!p) return !q;
+
+        if (!q) return !p;
+
+        if (p->val != q->val) {
+          return false;
+        }
+
+        return dfs(p->left, q->left) && dfs(p->right, q->right);
     }
 
     bool isSameTree(TreeNode* p, TreeNode* q) {
