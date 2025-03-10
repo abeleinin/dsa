8947800dd8f78cee202faf26a36a2c8d8d1940f1
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-03-10 13:17:07 -0500

    leetcode 200

--- neetcode150/medium/200-Number-of-Islands.cpp
+++ neetcode150/medium/200-Number-of-Islands.cpp
@@ -1,18 +1,19 @@
-#include <vector>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
 /**
- * \c IterativeSolution
+ * \c IterativeDfsSolution
  * 
- * 1. Use iterative dfs on island groups
- * 2. Set values to '0' after visiting
+ * 1. Use iterative DFS on island groups
+ * 2. Set values to '0' immediately after popping
  * 3. Check all 4 adjacent directions and push to the stack
  * 
  * Time  : O(m * n)
  * Space : O(min(m, n))
  */
-class IterativeSolution {
+class IterativeDfsSolution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int islands = 0;
@@ -20,32 +21,31 @@ public:
         int m = grid[0].size();
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                if (grid[i][j] == '0') {
-                    continue;
-                }
+                if (grid[i][j] == '0') continue;
                 
                 ++islands;
 
                 // Depth first search
-                vector<pair<int, int>> q = {{i, j}};
+                stack<pair<int, int>> stack;
+                stack.push({i, j});
                 int x, y;
-                while (!q.empty()) {
-                    tie(x, y) = q.back();
-                    q.pop_back();
+                while (!stack.empty()) {
+                    tie(x, y) = stack.top();
+                    stack.pop();
 
                     grid[x][y] = '0';
 
                     if (x-1 >= 0 && grid[x-1][y] == '1') {
-                        q.push_back({x-1, y});
+                        stack.push({x-1, y});
                     } 
                     if (x+1 < n && grid[x+1][y] == '1') {
-                        q.push_back({x+1, y});
+                        stack.push({x+1, y});
                     } 
                     if (y-1 >= 0 && grid[x][y-1] == '1') {
-                        q.push_back({x, y-1});
+                        stack.push({x, y-1});
                     } 
                     if (y+1 < m && grid[x][y+1] == '1') {
-                        q.push_back({x, y+1});
+                        stack.push({x, y+1});
                     }
                 }
             }
@@ -56,39 +56,35 @@ public:
 };
 
 /**
- * \c RecursiveSolution
+ * \c RecursiveDfsSolution
  * 
  * 1. Use recursive dfs on island groups
- * 2. Set values to '0' after visiting
- * 3. Check all 4 adjacent directions and push to the stack
+ * 2. Set values to '0' immediately upon processing
+ * 3. Recursively check all 4 adjacent directions
  * 
  * Time  : O(m * n)
- * Space : O(min(m, n))
+ * Space : O(m * n)
  */
-class Solution {
+class RecursiveDfsSolution {
 public:
-    void dfs(vector<vector<char>>& grid, const pair<int, int>& coord) {
-        int x = coord.first;
-        int y = coord.second;
-        if (grid[x][y] == '0') {
-            return;
-        }
+    void dfs(vector<vector<char>>& grid, int x, int y) {
+        if (grid[x][y] == '0') return;
     
         int n = grid.size();
         int m = grid[0].size();
         grid[x][y] = '0';
     
         if (x-1 >= 0 && grid[x-1][y] == '1') {
-            dfs(grid, {x-1, y});
+            dfs(grid, x-1, y);
         } 
         if (x+1 < n && grid[x+1][y] == '1') {
-            dfs(grid, {x+1, y});
+            dfs(grid, x+1, y);
         } 
         if (y-1 >= 0 && grid[x][y-1] == '1') {
-            dfs(grid, {x, y-1});
+            dfs(grid, x, y-1);
         } 
         if (y+1 < m && grid[x][y+1] == '1') {
-            dfs(grid, {x, y+1});
+            dfs(grid, x, y+1);
         }
     
         return;
@@ -100,13 +96,62 @@ public:
         int m = grid[0].size();
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                if (grid[i][j] == '0') {
-                    continue;
-                }
+                if (grid[i][j] == '0') continue;
                     
                 ++islands;
     
-                dfs(grid, {i, j});
+                dfs(grid, i, j);
+            }
+        }
+    
+        return islands;
+    }
+};
+
+/**
+ * \c IterativeBfsSolution
+ * 
+ * 1. Use iterative BFS on island groups
+ * 2. Set values to '0' after pushing to the queue since neighboring cells
+ *    may be processed before it 
+ * 3. Check all 4 adjacent directions, push to the queue and mark as visited
+ * 
+ * Time  : O(m * n)
+ * Space : O(min(m, n))
+ */
+ class IterativeBfsSolution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int islands = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == '0') continue;
+                    
+                ++islands;
+    
+                // Breath first search
+                queue<pair<int, int>> queue;
+                queue.push({i, j});
+                grid[i][j] = '0';
+    
+                int x, y;
+                while (!queue.empty()) {
+                    tie(x, y) = queue.front();
+                    queue.pop();
+    
+                    for (int d = 0; d < 4; ++d) {
+                        int dx = x + dir[d][0], dy = y + dir[d][1];
+                        if (dx >= 0 && dx < n &&
+                            dy >= 0 && dy < m &&
+                            grid[dx][dy] == '1') {
+                            queue.push({dx, dy});
+                            grid[dx][dy] = '0';
+                        } 
+                    }
+                }
             }
         }
     
