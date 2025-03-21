13e69b7f3b9ba12abc3fe64a5a0f4788692e281e
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-03-20 20:28:37 -0500

    leetcode 572

--- neetcode150/easy/572-Subtree-of-Another-Tree.cpp
+++ neetcode150/easy/572-Subtree-of-Another-Tree.cpp
@@ -13,7 +13,7 @@ struct TreeNode {
 /**
  * \c Solution
  * 
- * 1. Create isSametree helper function (leetcode 100)
+ * 1. Create isSameTree helper function (leetcode 100)
  * 2. Use early return if the trees are the same
  * 3. Otherwise, check if the left or right children of root are the same as the subRoot
  * 
@@ -22,19 +22,19 @@ struct TreeNode {
  */
 class Solution {
 public:
-    bool isSametree(TreeNode* t1, TreeNode* t2) {
+    bool isSameTree(TreeNode* t1, TreeNode* t2) {
         if (!t1 || !t2) return !t1 && !t2;
     
         if (t1->val != t2->val) return false;
     
-        return isSametree(t1->left, t2->left) && isSametree(t1->right, t2->right);
+        return isSameTree(t1->left, t2->left) && isSameTree(t1->right, t2->right);
     }
     
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if (!root) return false;
     
         // early return saves uneccassary recursive calls
-        if (isSametree(root, subRoot)) return true;
+        if (isSameTree(root, subRoot)) return true;
     
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
