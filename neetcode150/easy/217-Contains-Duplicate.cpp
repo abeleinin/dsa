3234d717bec30515dd0fed4b2eab5a778ddab589
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-04-09 02:51:23 -0500

    leetcode 217

--- neetcode150/easy/217-Contains-Duplicate.cpp
+++ neetcode150/easy/217-Contains-Duplicate.cpp
@@ -1,36 +1,49 @@
 #include <vector>
-#include <set>
+#include <unordered_set>
 using namespace std;
 
-// Solution 1
-// Note: Slow in C++
-class Solution{
+/**
+ * \c SortSolution
+ * 
+ * 1. Sort the numbers
+ * 2. Check if the consecutive elements are the same
+ * 
+ * Time  : O(n log n)
+ * Space : O(1)
+ */
+class SortSolution {
 public:
-  // Check if the length of the set 
-  bool containsDuplicate(vector<int>& nums){
-    set<int> s;
-    for(int n : nums){
-      s.insert(n);
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i + 1 < nums.size(); i++) {
+            if (nums[i] == nums[i+1]) return true;
+        }
+
+        return false;
     }
-    return s.size() != nums.size();
-  }
 };
 
-// Solution 2
-class Solution{
+/**
+ * \c SetSolution
+ * 
+ * 1. Use a std::unordered_set
+ * 2. std::unordered_set::insert returns pair<iterator, bool>, when pair::second is true 
+ *    the new element was sucessfully inserted (meaning it is not in the set)
+ * 
+ * Time  : O(n)
+ * Space : O(n)
+ */
+class SetSolution {
 public:
-  bool containsDuplicate(vector<int>& nums){
-    // Sort the vector
-    sort(nums.begin(), nums.end());
-    int x = nums[0];
-    // if n == n + 1 then the vector contains a duplicate
-    for(int i = 1; i < nums.size(); i++){
-      if(x == nums[i]){
-        return true;
-      }
-      // set n = n + 1
-      x = nums[i];
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+    
+        for (int n : nums) {
+            auto [_, inserted] = seen.insert(n);
+            if (!inserted) return true;
+        }
+    
+        return false;
     }
-    return false;
-  }
 };
