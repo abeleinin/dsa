a583dc44a2b2a430e0c69d9a4fa48b46e14ff8de
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-04-11 03:28:18 -0500

    leetcode 121

--- neetcode150/easy/121-Best-Time-to-Buy-and-Sell-Stock.cpp
+++ neetcode150/easy/121-Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -5,6 +5,9 @@ using namespace std;
 /**
  * \c Solution1
  * 
+ * 1. Maintain min_val within current window, if you find a smaller value update 
+ * 2. Keep track of the max profit
+ * 
  * Time  : O(n)
  * Space : O(1)
  */
@@ -12,42 +15,16 @@ class Solution1 {
 public:
     int maxProfit(vector<int>& prices) {
         int min_val = prices[0];
-        int price = 0;
-        for(int i = 1; i < prices.size(); i++){
+        int profit = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
             int curr = prices[i] - min_val;
-            if(curr > price){
-                price = curr;
-            } else if(prices[i] < min_val){
+            if (curr > profit) {
+                profit = curr;
+            } else if (prices[i] < min_val) {
                 min_val = prices[i];
             }
         }
-        return price;
-    }
-};
-
-/**
- * \c Solution2
- * 
- * Time  : O(n)
- * Space : O(1)
- */
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int res = 0;
-        int curr = 0;
-
-        int i = 0;
-        int j = 1;
-        while (j <= prices.size()-1) {
-            if (prices[i] < prices[j]) {
-                curr = prices[j] - prices[i];
-            } else {
-                i = j;
-            }
-            res = max(res, curr);
-            ++j;
-        }
-        return res;
+        return profit;
     }
 };
