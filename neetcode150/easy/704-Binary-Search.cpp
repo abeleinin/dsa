3fa83825b2c9041856e5838701b2ce6b2d1b75fd
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-04-14 10:16:27 -0500

    leetcode 704

--- neetcode150/easy/704-Binary-Search.cpp
+++ neetcode150/easy/704-Binary-Search.cpp
@@ -1,21 +1,69 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+/**
+ * \c IterativeSolution
+ * 
+ * 1. Use two pointers
+ * 2. Calculate middle as (high + low) / 2
+ * 3. Update low or high depending on whether curr is greater or less than target (use middle)
+ * 
+ * Time  : O(log n)
+ * Space : O(1)
+ */
+class IterativeSolution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target) {
-                return mid;
-            } else if(nums[mid] > target) {
-                right = mid - 1;
+        int low = 0;
+        int high = nums.size()-1;
+    
+        int curr, mid;
+        while (low <= high) {
+            mid = (high + low) / 2;
+            curr = nums[mid];
+                
+            if (curr == target) return mid;
+                
+            if (curr < target) {
+                low = mid + 1;
             } else {
-                left = mid + 1;
+                high = mid - 1;
             }
         }
         return -1;
+    }
+};
+
+/**
+ * \c RecursiveSolution
+ * 
+ * 1. Use recursive util function with high/low pointers
+ * 2. Calculate middle value from high/low
+ * 3. Update high/low in recursive calls dependent on whether curr is greater or less than target (use middle)
+ * 
+ * Time  : O(log n)
+ * Space : O(1)
+ */
+class RecursiveSolution {
+public:
+    int recursive_search(vector<int>& nums, int target, int low, int high) {
+        int curr, mid;
+        if (low <= high) {
+            mid = (high + low) / 2;
+            curr = nums[mid];
+    
+            if (curr == target) return mid;
+    
+            if (curr < target) {
+                return recursive_search(nums, target, mid+1, high);
+            } else {
+                return recursive_search(nums, target, low, mid-1);
+            }
+        }
+        return -1;
+    }
+    
+    int search(vector<int>& nums, int target) {
+        return recursive_search(nums, target, 0, nums.size()-1);
     }
 };
