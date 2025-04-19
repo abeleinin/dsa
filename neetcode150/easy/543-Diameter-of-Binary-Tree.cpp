a401f38af6d2bd33143b99d57dbd006214d82861
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-04-19 13:05:29 -0500

    leetcode 543

--- neetcode150/easy/543-Diameter-of-Binary-Tree.cpp
+++ neetcode150/easy/543-Diameter-of-Binary-Tree.cpp
@@ -12,23 +12,33 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+/**
+ * \c Solution
+ * 
+ * 1. Use dfs
+ * 2. Maintain a global maxDiameter
+ * 3. The current depth is the depth of the left and right j
+ * 4. return max(left, right) + 1, to compute the diameter through a node
+ * 
+ * Time  : O()
+ * Space : O()
+ */
 class Solution {
 public:
-    int val = 0;
-
+    int maxDiameter = 0;
+    
     int dfs(TreeNode* root) {
-        if (root == NULL) {
-            return -1;
-        }
+        if (!root) return 0;
+            
         int left = dfs(root->left);
         int right = dfs(root->right);
-        val = max(val, right + left + 2);
-
+        maxDiameter = max(maxDiameter, right + left);
+    
         return max(left, right) + 1;
     }
-
+    
     int diameterOfBinaryTree(TreeNode* root) {
         dfs(root);
-        return val;
+        return maxDiameter;
     }
 };
