dda9e741c9ab82a357851b976218625816bb272e
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-05-08 23:43:49 -0500

    leetcode 136

--- neetcode150/easy/136-Single-Number.cpp
+++ neetcode150/easy/136-Single-Number.cpp
@@ -2,11 +2,24 @@
 
 using namespace std;
 
+/**
+ * \c Solution
+ * 
+ * 1. Solve using bitwise XOR op and the following properties:
+ * 
+ *    a ^ a = 0 
+ *    a ^ 0 = a
+ * 
+ * 2. Therefore, the solution is simply using XOR over the input
+ * 
+ * Time  : O(n)
+ * Space : O(1)
+ */
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int res = 0;
-        for (int i = 0; i < nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++) {
             res ^= nums[i];
         }
         return res;
