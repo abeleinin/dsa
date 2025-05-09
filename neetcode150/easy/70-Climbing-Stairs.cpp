b6cfce833a05600f21eb3c99c54e08996e308cee
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-05-08 19:02:54 -0500

    leetcode 70

--- neetcode150/easy/70-Climbing-Stairs.cpp
+++ neetcode150/easy/70-Climbing-Stairs.cpp
@@ -1,23 +1,41 @@
 #include <unordered_map>
 
-// O(n) time, O(n) space solution
-class Solution {
+using namespace std;
+
+/**
+ * \c MapSolution
+ * 
+ * 1. Create a map containing base cases for 1 and 2 stairs
+ * 2. Base case becomes if the number is within the map
+ * 3. Cache each nth stair in the map
+ * 4. Return the result
+ * 
+ * Time  : O(n)
+ * Space : O(n)
+ */
+class MapSolution {
 public:
-    std::unordered_map<int, int> stairs = {{1, 1}, {2, 2}};
+    unordered_map<int, int> mp = {{1, 1}, {2, 2}};
+    
     int climbStairs(int n) {
-        if (n < 0) {
-            return 0;
-        }
-        if (stairs.find(n) != stairs.end()){
-            return stairs[n];
-        }
-        stairs[n] = climbStairs(n - 1) + climbStairs(n - 2);
-        return stairs[n];
+        if (mp.find(n) != mp.end()) return mp[n];
+    
+        mp[n] = climbStairs(n-1) + climbStairs(n-2);
+    
+        return mp[n];
     }
 };
 
-// O(n) time, O(1) space solution
-class Solution {
+/**
+ * \c OptimalSolution
+ * 
+ * 1. Keep track of the prev and curr value
+ * 2. For each stair, save curr as temp, update curr from prev, update prev from temp
+ * 
+ * Time  : O(n)
+ * Space : O(1)
+ */
+class OptimalSolution {
 public:
     int climbStairs(int n) {
         if (n == 1) {
@@ -28,11 +46,12 @@ public:
         int curr = 1;
         int prev = 0;
 
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             temp = curr;
             curr += prev;
             prev = temp;
         }
+
         return curr;
     }
 };
