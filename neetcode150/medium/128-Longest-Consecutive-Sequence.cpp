2bc8aacad965233419087ce3eb1179991d246c67
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-05-12 12:05:15 -0500

    leetcode 128

--- neetcode150/medium/128-Longest-Consecutive-Sequence.cpp
+++ neetcode150/medium/128-Longest-Consecutive-Sequence.cpp
@@ -1,56 +1,33 @@
 #include <vector>
-#include <set>
+#include <unordered_set>
 
 using namespace std;
 
 /**
- * \c Solution1
+ * \c Solution
  * 
  * 1. Creates a set of the input vector.
  * 2. Checks if the given number is the "beginning" of a sequence (ie num-1 is not contained in the set)
- * 3. If it's the "beginning", count the sequence length using while loop
- */
-class Solution1 {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        set<int> s(nums.begin(), nums.end());
-        int res = 0;
-        int curr = 0;
-        for (int n : s) {
-            if (s.find(n - 1) == s.end()) {
-                curr = 1; 
-                while(s.find(n + 1) != s.end()){
-                    curr++;
-                    n++;
-                }
-                res = max(curr, res);
-            }
-        }
-        return res;
-    }
-};
-
-/**
- * \c Solution2
+ * 3. If it's the "beginning" of a sequence, count the sequence length using while loop
  * 
- * Same approach as \c Solution1, but uses std::set<>::contains, added in C++20
- * https://en.cppreference.com/w/cpp/container/set/contains
+ * Uses std::unordered_set<>::contains, added in C++20
+ * https://en.cppreference.com/w/cpp/container/unordered_set/contains
+ * 
+ * Time  : O(n)
+ * Space : O(n)
  */
-class Solution2 {
+class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        set<int> s(nums.begin(), nums.end());
-
+        unordered_set<int> unique(nums.begin(), nums.end());
+    
         int res = 0;
-        for (int num : s) {
-            if (!s.contains(num-1)) {
-                int curr = 1;
-                while (s.contains(num+1)) {
-                    ++curr;
-                    ++num;
-                }
-                res = max(curr, res);
-            }
+        for (int num : unique) {
+            if (unique.contains(num-1)) continue;
+    
+            int count = 1;
+            while (unique.contains(++num)) ++count;
+            res = max(count, res);
         }
         return res;
     }
