ed25c5d403d2e3e354b64ace51135772e2f5aef1
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-05-11 23:24:48 -0500

    leetcode 347

--- neetcode150/medium/347-Top-K-Frequent-Elements.cpp
+++ neetcode150/medium/347-Top-K-Frequent-Elements.cpp
@@ -3,32 +3,40 @@
 
 using namespace std;
 
+/**
+ * \c Solution
+ * 
+ * 1. Create frequency count map for each value in the input
+ * 2. Sort these frequencies using bucket sort where the 
+ *    number of buckets is the size of the input+1
+ * 3. Since the answer is guaranteed to be unique we can 
+ *    reverse iterate through the buckets until k is 0
+ * 
+ * Time  : O(n)
+ * Space : O(n)
+ */
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int, int> h;
+        unordered_map<int, int> counts;
+    
         for (int n : nums) {
-            h[n] += 1;
+            counts[n]++;
         }
-
-        vector<vector<int> > b(nums.size()+1);
-        for (auto& [n, v] : h) {
-            b[v].push_back(n);
+    
+        vector<vector<int>> bin(nums.size()+1);
+        for (auto [key, val] : counts) {
+            bin[val].push_back(key);
         }
-
-        vector<int> result;
-        for (int i = b.size()-1; i > 0; --i) {
-            vector<int> bucket = b[i];
-            while (!bucket.empty()) {
-                result.push_back(bucket.back());
-                bucket.pop_back();
+    
+        vector<int> res;
+        for (int i = bin.size()-1; i > -1; --i) {
+            for (int n : bin[i]) {
+                res.push_back(n);
                 --k;
-                if (k == 0) {
-                    return result;
-                }
+                if (k == 0) return res;
             }
         }
-        
-        return result;
+        return res;
     }
 };
