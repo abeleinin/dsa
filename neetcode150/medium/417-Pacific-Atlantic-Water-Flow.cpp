be15bc1db6d89aa632f1aa6a846160e0fac4b615
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-05-21 23:54:07 -0500

    leetcode 417

--- neetcode150/medium/417-Pacific-Atlantic-Water-Flow.cpp
+++ neetcode150/medium/417-Pacific-Atlantic-Water-Flow.cpp
@@ -1,49 +1,72 @@
-#include <vector>
+#include <queue>
+#include <set>
 
 using namespace std;
 
-void dfs(vector<vector<int>>& heights, vector<vector<bool>>& vis, int i, int j)
-{
-    static const vector<pair<int,int>> directions = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
-
-    vis[i][j] = true;
-
-    for (auto d : directions) {
-        if (0 <= i+d.first && i+d.first < heights.size() &&
-            0 <= j+d.second && j+d.second < heights[0].size() &&
-            heights[i][j] <= heights[i+d.first][j+d.second])
-        {
-            if (!vis[i+d.first][j+d.second]) {
-                dfs(heights, vis, i+d.first, j+d.second);
+/**
+ * \c Solution
+ * 
+ * 1. Use the edges of the heights as starting nodes of a graph
+ * 2. Perform BFS on each node collecting the set of pacific and alantic nodes
+ * 3. Find the intersection of the two sets
+ * 
+ * Time  : O(m*n)
+ * Space : O(m)
+ */
+class Solution {
+public:
+    void bfs(const vector<vector<int>>& heights, 
+                queue<pair<int, int>>& q,
+                set<pair<int, int>>& ocean) {
+            
+        static const int dir[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        set<pair<int,int>> visited;
+        while(!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+    
+            ocean.insert({x, y});
+    
+            for (int d = 0; d < 4; d++) {
+                int dx = x + dir[d][0], dy = y + dir[d][1];
+                if (dx >= 0 && dx < heights.size() &&
+                    dy >= 0 && dy < heights[0].size() &&
+                    !visited.count({dx, dy}) &&
+                    heights[x][y] <= heights[dx][dy]) {
+                    visited.insert({dx, dy});
+                    q.push({dx, dy});
+                }
             }
         }
     }
-
-    return;
-}
-
-class Solution {
-public:
+    
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        vector<vector<int>> res;
-        vector<vector<bool>> pac(heights.size(), vector<bool>(heights[0].size(), false));
-        vector<vector<bool>> atl(heights.size(), vector<bool>(heights[0].size(), false));
-
-        for (int j = 0; j < heights[0].size(); j++){
-            dfs(heights, pac, 0, j);
-            dfs(heights, atl, heights.size()-1, j);
-        }
-
-        for (int i = 0; i < heights.size(); i++){
-            dfs(heights, pac, i, 0);
-            dfs(heights, atl, i, heights[0].size()-1);
-        }
-
-        for (int i = 0; i < heights.size(); i++){
-            for (int j = 0; j < heights[0].size(); j++){
-                if (pac[i][j] && atl[i][j]) {
-                    res.push_back({i, j});
+        queue<pair<int, int>> pq;
+        queue<pair<int, int>> aq;
+    
+        int n = heights.size();
+        int m = heights[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || j == 0) {
+                    pq.push({i, j});
                 }
+                if (i == n-1 || j == m-1) {
+                    aq.push({i, j});
+                }
+            }
+        }
+    
+        set<pair<int, int>> pacific;
+        set<pair<int, int>> atlantic;
+    
+        bfs(heights, pq, pacific);
+        bfs(heights, aq, atlantic);
+    
+        vector<vector<int>> res;
+        for (auto pair : pacific) {
+            if (atlantic.find(pair) != atlantic.end()) {
+                res.push_back({pair.first, pair.second});
             }
         }
         return res;
